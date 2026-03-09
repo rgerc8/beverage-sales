809761bae00b12af1e7bf75eb8d603a4495fc552
rgerc8 <ruben.gabriel.carvalho@gmail.com>
2026-03-09 16:01:56 +0000

    Refactor notebook to use PySpark for data processing and update data download path in script
    - Updated the Jupyter notebook (02-teste.ipynb) to initialize a Spark session and display a range of numbers using PySpark.
    - Changed the output format to show the result in a structured table.
    - Modified the data download script (get-data.py) to set the destination directory to a Windows path.

--- scripts/get-data.py
+++ scripts/get-data.py
@@ -7,7 +7,8 @@ src_path = Path(kagglehub.dataset_download("sebastianwillmann/beverage-sales"))
 print("Downloaded to cache:", src_path)
 
 # Destination inside your project
-dst_dir = Path("/home/rgerc/Projects/beverage-sales/data/raw")
+#dst_dir = Path("/home/rgerc/Projects/beverage-sales/data/raw")
+dst_dir = Path("C:/Users/m24810/Projects/beverage-sales/data/raw")
 # If it is the first time, you may want to create the destination directory:
 # dst_dir.mkdir(parents=True, exist_ok=True)
 
